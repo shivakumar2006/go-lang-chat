64b5a08a06f66013e1d073a87b70c571d98bfd39
shivakumar2006 <official.shivakumar2006@gmail.com>
2024-08-18 17:25:21 +0530

    commit

--- backend/pkg/websocket/client.go
+++ backend/pkg/websocket/client.go
@@ -14,8 +14,8 @@ type Client struct {
 }
 
 type Message struct {
-	Type int    `json: "type"`
-	Body string `json: "body"`
+	Type int    `json:"type"`
+	Body string `json:"body"`
 }
 
 func (c *Client) Read() {
