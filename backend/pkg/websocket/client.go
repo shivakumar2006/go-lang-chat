537d4712beb80b5f995c7fab00c99c21343b865a
shivakumar2006 <official.shivakumar2006@gmail.com>
2024-08-18 17:25:03 +0530

    commit

---
+++ backend/pkg/websocket/client.go
@@ -0,0 +1,37 @@
+package websocket
+
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+type Client struct {
+	ID   string
+	Conn *websocket.conn
+	Pool *Pool
+	mu   sync.Mutex
+}
+
+type Message struct {
+	Type int    `json: "type"`
+	Body string `json: "body"`
+}
+
+func (c *Client) Read() {
+	defer func() {
+		c.Pool.Unergister <- c
+		c.Conn.Close()
+	}()
+
+	for {
+		messageType, p, err := c.Conn.ReadMessage()
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		message := Message{Type: messageType, Body: string(p)}
+		c.Pool.Broadcast <- message
+		fmt.Printf("message received %+v\n", message)
+	}
+}
